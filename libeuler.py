fa4038b6438e5ee0628d4fe2a693c985b4349482
Michael Foukarakis <foukarakis.michael@gmail.com>
2012-01-18 12:00:46 +0200

    Replaced the prime sieve with one using numpy - at least ~8 times faster.

--- libeuler.py
+++ libeuler.py
@@ -3,7 +3,7 @@
 # @author      Michael Foukarakis
 # @version     0.1
 # @date        Created:     Tue Oct 11, 2011 08:51 GTB Daylight Time
-#              Last Update: Mon Jan 24, 2011 14:54 GTB Standard Time
+#              Last Update: Wed Jan 18, 2012 11:56 GTB Standard Time
 #------------------------------------------------------------------------
 # Description: Project Euler helper library
 #------------------------------------------------------------------------
@@ -157,14 +157,17 @@ def binomial(n, k):
     return nt
 
 # My awesome prime sieve.
+import numpy as np
 def prime_sieve(n):
-    ''' Returns  a list of primes < n '''
-    sieve = [True] * n
-    for i in range(3, int(n**0.5)+1, 2):
+    """ Input n >= 6, Returns a list of primes, 2 <= p < n """
+    sieve = np.ones(n/3 + (n%6==2), dtype=np.bool)
+    sieve[0] = False
+    for i in range(int(n**0.5)//3+1):
         if sieve[i]:
-            sieve[i*i::2*i] = [False] * int((n-i*i-1)/(2*i)+1)
-    return [2] + [i for i in range(3, n, 2) if sieve[i]]
-
+            k=3*i+1|1
+            sieve[      ((k*k)//3)      ::2*k] = False
+            sieve[(k*k+4*k-2*k*(i&1))//3::2*k] = False
+    return np.r_[2, 3, ((3 * np.nonzero(sieve)[0]+1)|1)].tolist()
 
 # Find the maximal path from the root of a tree to a leaf
 # t is a list of lists, representing the tree top-down
